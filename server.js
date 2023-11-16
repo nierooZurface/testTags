2749c6846036fa7c2470f9cd944170dce19e1805
nieroo selva <no@zurface.com>
2023-11-16 10:24:59 +0100

    added first change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! \n\n My site! \n\n I am the author!');
+  res.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
